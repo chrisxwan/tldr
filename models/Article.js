4513f9f4195fa590601a06265aba274820c8b849
Christopher Wan <christopher.wan@yale.edu>
2015-06-07 09:10:33 -0400

    ignore bower

---
+++ models/Article.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+  id: mongoose.Schema.ObjectId,
+  address: String,
+  title: String,
+  sentences: [ {type: String}],
+});
+
+
+module.exports = mongoose.model('Article', articleSchema);
+// mongoose.connect('mongodb://localhost/test');
