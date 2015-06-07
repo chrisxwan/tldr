8bad2444c7585c2f1d4395dcc58b4f2be84a65d3
Christopher Wan <christopher.wan@yale.edu>
2015-06-07 00:02:35 -0400

    tldr article, need to see if callbacks work, render error and article pages

--- routes/index.js
+++ routes/index.js
@@ -15,15 +15,30 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     var address = req.body.url;
     var tldr = function() {
-        var sentences = [];
-        $('p').each(function() {
-            var paragraph = $(this).text();
-            var period = paragraph.indexOf('.');
-            var sentence = paragraph.substring(0, period + 1);
-            sentences.push(sentence);
+        page.evaluate(function() {
+            var sentences = [];
+            $('p').each(function() {
+                var paragraph = $(this).text();
+                var period = paragraph.indexOf('.');
+                var sentence = paragraph.substring(0, period + 1);
+                sentences.push(sentence);
+            });
+            var title = $('title').text();
+            window.callPhantom({
+                sentences: sentences,
+                title: title
+            });
         });
-        return sentences;
+        phantom.exit();
     }
+
+    page.onCallback = function(data) {
+        res.render('/' + title, {
+            sentences: sentences,
+            title: title
+        });
+    }
+
     page.open(address, function(status) {
         if(status !== 'success') {
             res.render('/error', {
@@ -33,12 +48,7 @@ router.post('/', function(req, res) {
         }
         else {
             page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {   
-                var returnSentences = page.evaluate(function() {
-                    return tldr();
-                });
-                res.render('/' + address, {
-                    sentences: returnSentences;
-                });
+                tldr();
             });
         }
     });
