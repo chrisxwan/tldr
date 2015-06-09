1752a9395fa3c761a90db9c508a6740f6952a8ed
Christopher Wan <christopher.wan@yale.edu>
2015-06-08 23:29:56 -0400

    mvp baby

--- routes/index.js
+++ routes/index.js
@@ -22,53 +22,59 @@ router.post('/', function(req, res) {
                     res.redirect('/error');
                 }
                 else {
-                    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {   
-                        async.waterfall([
-                            function (callback) {
-                                var sentences;
-                                page.evaluate(function(){ 
-                                    var returnSentences = [];
-                                    $('p').each(function() {
-                                        var paragraph = $(this).text();
-                                        var period = paragraph.indexOf('.');
-                                        var sentence = paragraph.substring(0, period + 1);
-                                        returnSentences.push(sentence);
-                                    });
-                                    return returnSentences;
-                                }, function (result) {
-                                    sentences = result;
-                                    callback(null, sentences);
-                                });
-                            },
-                            function (sentences, callback) {
-                                var title;
-                                page.evaluate(function() {
-                                    return $('title').text();
-                                }, function (result) {
-                                    title = result;
-                                    callback(null, sentences, title);
-                                }); 
-                                
-                            },
-                            function (sentences, title, callback) {
-                                var newArticle = new Article({
-                                    address: address,
-                                    title: title,
-                                    sentences: sentences
-                                });
-                                newArticle.save(function (err) {
-                                    if(err) {
-                                        res.send('There was a problem adding to the database.');
+                    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {  
+                        req.db.articles.findOne({'address': address}, function (error, article) {
+                            if(!error && article) {
+                                res.redirect('/article' + article._id);
+                            } else {
+                                async.waterfall([
+                                    function (callback) {
+                                        var sentences;
+                                        page.evaluate(function(){ 
+                                            var returnSentences = [];
+                                            $('p').each(function() {
+                                                var paragraph = $(this).text();
+                                                var period = paragraph.indexOf('.');
+                                                var sentence = paragraph.substring(0, period + 1);
+                                                returnSentences.push(sentence);
+                                            });
+                                            return returnSentences;
+                                        }, function (result) {
+                                            sentences = result;
+                                            callback(null, sentences);
+                                        });
+                                    },
+                                    function (sentences, callback) {
+                                        var title;
+                                        page.evaluate(function() {
+                                            return $('title').text();
+                                        }, function (result) {
+                                            title = result;
+                                            callback(null, sentences, title);
+                                        }); 
+                                        
+                                    },
+                                    function (sentences, title, callback) {
+                                        var newArticle = new Article({
+                                            address: address,
+                                            title: title,
+                                            sentences: sentences
+                                        });
+                                        newArticle.save(function (err) {
+                                            if(err) {
+                                                res.send('There was a problem adding to the database.');
+                                            }
+                                        });    
+                                        res.redirect('/article' + newArticle._id);
+                                        ph.exit();    
+                                        callback(null, 'done');                       
                                     }
-                                });    
-                                res.redirect('/article' + title);
-                                ph.exit();    
-                                callback(null, 'done');                       
+                                ], 
+                                function (err, results) {
+                                    if (err) { console.log(err); }
+                                });        
                             }
-                        ], 
-                        function (err, results) {
-                            if (err) { console.log(err); }
-                        });
+                        }); 
                     });
                 }
             });
@@ -80,9 +86,8 @@ router.get('/error', function(req, res){
     res.render('error');
 })
 
-router.get('/article:title', function(req, res) {
-    req.db.article.findOne({title: req.params.title}, function (error, article) {
-        console.log(article.address);
+router.get('/article:id', function(req, res) {
+    Article.findById(req.params.id, function (error, article) {
         res.render('tldr', {
             address: article.address,
             articleTitle: article.title,
