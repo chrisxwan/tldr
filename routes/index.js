4fec2d5b28e9927e93d618a7937aaf377bf1250d
Christopher Wan <christopher.wan@yale.edu>
2015-06-12 00:40:53 -0400

    refine algo

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,28 @@ var Article = mongoose.model('Article');
 var router = express.Router();
 
 var abbreviations = ['Mr.', 'Sr.', 'Ms.', 'Jr.', 'Prof.', 'Gen.', 'Rep.', 'Sen.', 'Mrs.',
-                     'Ph.D.', 'M.D.', 'B.A.', 'M.A.', 'D.D.S.', 'in.', 'ft.', 
+                     'Ph.D.', 'M.D.', 'B.A.', 'M.A.', 'in.', 'ft.', 
                      'a.m.', 'p.m.', 'U.S.', 'etc.', 'i.e.', 'al.', 'e.g.', 
                      'B.C.', 'A.D.', 'C.E.', 'A.M.', 'P.M.', 'D.C.', 'Lt.', 'Col.'];
+
+var nth_occurrence = function (string, char, nth) {
+    var first_index = string.indexOf(char);
+    var length_up_to_first_index = first_index + 1;
+
+    if (nth == 1) {
+        return first_index;
+    } else {
+        var string_after_first_occurrence = string.slice(length_up_to_first_index);
+        var next_occurrence = nth_occurrence(string_after_first_occurrence, char, nth - 1);
+
+        if (next_occurrence === -1) {
+            return -1;
+        } else {
+            return length_up_to_first_index + next_occurrence;  
+        }
+    }
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -35,11 +54,21 @@ router.post('/', function(req, res) {
                                 async.waterfall([
                                     function (callback) {
                                         var sentences;
-                                        page.evaluate(function(){ 
+                                        page.evaluate(function (abbreviations, nth_occurrence){ 
                                             var returnSentences = [];
                                             $('p').each(function() {
+                                                var nth = 1;
                                                 var paragraph = $(this).text();
-                                                var period = paragraph.indexOf('.');
+                                                var period = nth_occurrence(paragraph, '.', nth);
+                                                var parse = paragraph.substring(period-4, period+4);
+                                                for(i = 0; i < abbreviations.length; i++) {
+                                                    if(parse.indexOf(abbreviations[i]) >= 0) {
+                                                        nth++;
+                                                        period = nth_occurrence(paragraph, '.', nth);
+                                                        i=0;
+                                                        var parse = paragraph.substring(period-4, period+4);
+                                                    }
+                                                }
                                                 var sentence = paragraph.substring(0, period + 1);
                                                 returnSentences.push(sentence);
                                             });
@@ -47,7 +76,7 @@ router.post('/', function(req, res) {
                                         }, function (result) {
                                             sentences = result;
                                             callback(null, sentences);
-                                        });
+                                        }, abbreviations, nth_occurrence);
                                     },
                                     function (sentences, callback) {
                                         var title;
