9eb5ff7d7b79eb928f975664de2b8e17c1679553
Christopher Wan <christopher.wan@yale.edu>
2015-06-20 23:39:15 -0400

    rollback

--- routes/index.js
+++ routes/index.js
@@ -30,9 +30,9 @@ router.post('/', function(req, res) {
                             } else {
                                 async.waterfall([
                                     function (callback) {
-                                        page.evaluate(function(){
+                                        page.evaluate(function(){ 
                                             var abbreviations = ['Mr.', 'Sr.', 'Ms.', 'Jr.', 'Prof.', 'Gen.', 'Rep.', 'Sen.', 'Mrs.',
-                                                                 'Ph.D.', 'M.D.', 'B.A.', 'M.A.', 'in.', 'ft.', 'F.B.I.',
+                                                                 'Ph.D.', 'M.D.', 'B.A.', 'M.A.', 'in.', 'ft.', "F.B.I."
                                                                  'a.m.', 'p.m.', 'U.S.', 'etc.', 'i.e.', 'al.', 'e.g.', 
                                                                  'B.C.', 'A.D.', 'C.E.', 'A.M.', 'P.M.', 'D.C.', 'Lt.', 'Col.'];
                                             var nth_occurrence = function (string, char, nth) {
@@ -51,59 +51,26 @@ router.post('/', function(req, res) {
                                                         return length_up_to_first_index + next_occurrence;  
                                                     }
                                                 }
-                                            };
-                                            var inTextBlock = false;
-                                            var level = Number.MAX_VALUE;
-                                            var textBlocks = [];
-                                            var index = -1;
-                                            $('html').each(function() {
-                                                console.log('hi');
-                                                var currentLevel = $(this).parents().length;
-                                                if(inTextBlock && currentLevel <= level) {
-                                                    inTextBlock = false;
-                                                }
-                                                if($(this).is('p') && !inTextBlock) {
-                                                    inTextBlock = true;
-                                                    level = $(this).parents().length - 1;
-                                                    var block = { sentences: [], characters: 0 };
-                                                    index++;
-                                                    textBlocks.push(block);
-                                                }
-                                                if($(this).is('p') && inTextBlock) {
-                                                    var paragraphLength = $(this).text().length;
-                                                    textBlocks[index].characters += paragraphLength;
-                                                    textBlocks[index].sentences.push($(this).text());
-                                                }
-                                            });
-                                            var maxIndex = -1;
-                                            var maxChars = Number.MIN_VALUE;
-                                            for(i = 0; i < textBlocks.length; i++) {
-                                                if(textBlocks[i].characters > maxChars) {
-                                                    maxIndex = i;
-                                                    maxChars = textBlocks[i].characters;
-                                                }
                                             }
-                                            var article = textBlocks[maxIndex].sentences;
-                                            for(i=0; i < article.length; i++) {
-                                                var nth = 1;
-                                                var paragraph = article[i];
-                                                var period = nth_occurrence(paragraph, '.', nth);
-                                                var parse = paragraph.substring(period-4, period+3);
-                                                if(i === article.length - 1) {
-                                                    returnSentences.push(paragraph);
-                                                } else {
-                                                    for(j = 0; j < abbreviations.length; j++) {
-                                                        if(parse.indexOf(abbreviations[j]) >= 0) {
+                                            var returnSentences = [];
+                                            $('p').each(function() {
+                                                if(!($(this).is('.comment-text'))) {
+                                                    var nth = 1;
+                                                    var paragraph = $(this).text();
+                                                    var period = nth_occurrence(paragraph, '.', nth);
+                                                    var parse = paragraph.substring(period-4, period+3);
+                                                    for(i = 0; i < abbreviations.length; i++) {
+                                                        if(parse.indexOf(abbreviations[i]) >= 0) {
                                                             nth++;
                                                             period = nth_occurrence(paragraph, '.', nth);
-                                                            j=0;
+                                                            i=0;
                                                             var parse = paragraph.substring(period-4, period+3);
                                                         }
                                                     }
+                                                    var sentence = paragraph.substring(0, period + 1);
+                                                    returnSentences.push(sentence);
                                                 }
-                                                var sentence = paragraph.substring(0, period + 1);
-                                                returnSentences.push(sentence);
-                                            }
+                                            });
                                             return returnSentences;
                                         }, function (result) {
                                             sentences = result;
